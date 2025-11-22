470db7c06ccfd8c772dc17921bca35510b9be67e
M-HOSSAIN <mrmotaleb002@gmail.com>
2025-11-22 08:33:51 +0600

    Create foru.sh to set up Termux environment
    This script installs necessary packages and customizes the Termux environment for the user.

---
+++ foru.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+echo ""
+read -p $' \e[0m\e[1;92m Input your Username : \e[0m\e[1;96m\en' user
+echo ""
+echo ""
+echo -e  $g "Please  Wait  A few Minutes...... "
+echo ""
+apt update 
+apt upgrade -y 
+pkg install figlet -y
+pkg install python
+pkg install python2
+pip install pyfiglet
+pip install termcolor
+pkg install ncurses-utils -y 
+pkg install ruby -y 
+gem install lolcat 
+figlet -f big Wait | lolcat
+git clone https://github.com/shadow-x404/termux-banner.git
+sed 's+THBD+'$user'+g' simple-edit/bash.bashrc > /data/data/com.termux/files/usr/etc/bash.bashrc
+sed 's+THBD+'$user'+g' simple-edit/wlc.py > /data/data/com.termux/files/usr/etc/wlc.py
+rm -rf simple-edit
+figlet -f big M☞Termux-Hacker | lolcat
